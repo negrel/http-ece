a9eceb67586f8bbce5815ebf7d947c5fe66f3662
Alexandre Negrel <negrel.dev@protonmail.com>
2023-07-12 09:55:54 +0000

    chore: remove unused std/crypto dependency

--- deps.ts
+++ deps.ts
@@ -1,4 +1,3 @@
-export { crypto } from "https://deno.land/std@0.193.0/crypto/mod.ts";
 export * as base64url from "https://deno.land/std@0.193.0/encoding/base64url.ts";
 export * as base64 from "https://deno.land/std@0.193.0/encoding/base64.ts";
 
