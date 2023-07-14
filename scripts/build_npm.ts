e7d04f38df7c436111a70b86f65b1d1d23c0a805
Tom Jenkinson <tom@tjenkinson.me>
2023-07-14 21:43:25 +0200

    configure github actions workflow and publish to npm
    closes #3

---
+++ scripts/build_npm.ts
@@ -0,0 +1,33 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.37.0/mod.ts";
+
+const version = Deno.args[0];
+if (!version) throw new Error('Missing version');
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  compilerOptions: {
+    lib: ["ES2021", "DOM"],
+  },
+  test: false,
+  shims: {},
+  package: {
+    name: "http_ece",
+    version,
+    description: "An implementation of HTTP Encrypted Content-Encoding scheme (RFC 8188).",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/negrel/http_ece.git",
+    },
+    bugs: {
+      url: "https://github.com/negrel/http_ece/issues",
+    },
+  },
+  postBuild() {
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
