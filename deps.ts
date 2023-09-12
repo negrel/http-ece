5fe79171a225c3f667da0563ba071033f1590265
Alexandre Negrel <negrel.dev@protonmail.com>
2023-09-12 18:37:33 +0200

    update deno std lib to 0.201.0

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as base64url from "https://deno.land/std@0.193.0/encoding/base64url.ts";
-export * as base64 from "https://deno.land/std@0.193.0/encoding/base64.ts";
+export * as base64url from "https://deno.land/std@0.201.0/encoding/base64url.ts";
+export * as base64 from "https://deno.land/std@0.201.0/encoding/base64.ts";
 
-export * as bytes from "https://deno.land/std@0.193.0/bytes/mod.ts";
+export * as bytes from "https://deno.land/std@0.201.0/bytes/mod.ts";
